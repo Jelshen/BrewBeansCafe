9b570d9abb0ad8a483dc31abffeaa8a8443e9421
Jelshen Febryand Valesco <jelshenfebryandvalesco@gmail.com>
2025-06-05 11:53:59 +0700

    Added new Media folder, minor changes to vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    strictPort: false, // Allow Vite to use alternative ports
+    open: true, // Automatically open browser on server start
+  },
 });
